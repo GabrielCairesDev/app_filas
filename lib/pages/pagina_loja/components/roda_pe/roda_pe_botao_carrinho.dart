aa6b3f93955718889d467bc94ff3829fb0e8ebcb
Gabriel Caires <gabriel.araujo.caires@gmail.com>
2023-06-12 15:31:05 -0300

    finalização do básico

--- lib/pages/pagina_loja/components/roda_pe/roda_pe_botao_carrinho.dart
+++ lib/pages/pagina_loja/components/roda_pe/roda_pe_botao_carrinho.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../routes/rotas.dart';
+import '../../../pagina_carrinho/controller/controlador_pagina_carrinho.dart';
 
 class RodaPeBotaoCarrinho extends StatelessWidget {
   const RodaPeBotaoCarrinho({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controladorPaginaCarrinho = Provider.of<ControladorPaginaCarrinho>(context);
     return TextButton(
-      onPressed: () {},
+      onPressed: () {
+        if (controladorPaginaCarrinho.produtosSelecionados.value.isNotEmpty) {
+          Navigator.of(context).pushNamed(Rotas.paginaCarrinho);
+        }
+      },
       style: TextButton.styleFrom(
           backgroundColor: const Color(0xFF335AF3),
           fixedSize: const Size(165, 60),
